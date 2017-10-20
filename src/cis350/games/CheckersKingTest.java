2224f57f4a98c14055ee32b4c618ff97ba87b677
ajmaley <31752260+ajmaley@users.noreply.github.com>
2017-10-19 21:35:30 -0400

    Add files via upload
    Adding Checkers JUnit Test

---
+++ src/cis350/games/CheckersKingTest.java
@@ -0,0 +1,42 @@
+package cis350.games;
+
+import cis350.games.checkersBoard.Color;
+import cis350.games.checkersKing;
+import cis350.games.checkersPawn;
+import cis350.games.checkersStandardBoard;
+import junit.framework.TestCase;
+
+/**
+ * Test cases for the King Piece
+ 
+ *
+ */
+public class CheckersKingTest extends TestCase {
+	
+	/**
+	 * Test valid vertical move.
+	 */
+	public void testValidKingMove(){
+		checkersStandardBoard board = new checkersStandardBoard(8,8);
+		checkersKing newKing = new checkersKing(4, 4, Color.white, board);
+		assertTrue(newKing.canMove(6, 6));
+		assertTrue(newKing.canMove(2, 2));
+		assertTrue(newKing.canMove(6, 2));
+		assertTrue(newKing.canMove(2, 6));
+		assertTrue(newKing.canMove(3, 3));
+		assertTrue(newKing.canMove(5, 5));
+		assertTrue(newKing.canMove(3, 5));
+		assertTrue(newKing.canMove(5, 3));
+	}
+	
+	
+	/**
+	 * Test invalid King move.
+	 */
+	public void testInvalidKingMove(){
+		checkersStandardBoard board = new checkersStandardBoard(8,8);
+		checkersKing newKing = new checkersKing(3, 7, Color.black, board);
+		assertFalse(newKing.canMove(3, 5));
+	}
+
+}
