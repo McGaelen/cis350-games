85d90ee47789976aeae41b7720881bd25ddc7c16
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2017-12-07 15:59:52 -0500

    added package-info.java

---
+++ src/cis350/games/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains all the necessary classes to run
+ * Game Suite. Chess and Checkers are implemented using Swing,
+ * while Connect Four, Tic Tac Toe, and the Main Menu are
+ * implemented using JavaFX.
+ *
+ * It also contains the FXML files for the games implemented in JavaFX,
+ * as well as the controller for the Achievements screen.
+ */
+package cis350.games;
