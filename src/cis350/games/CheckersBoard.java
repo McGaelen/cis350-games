b12b72eda33cdbb53005e95429f11714eb9a18d3
ajmaley <31752260+ajmaley@users.noreply.github.com>
2017-12-07 17:24:58 -0500

    Add files via upload

---
+++ src/cis350/games/CheckersBoard.java
@@ -0,0 +1,127 @@
+package cis350.games;
+
+import cis350.games.CheckersBoard.Color;
+
+/**
+ * Represents a game board for a Checkers Game.
+ */
+public abstract class CheckersBoard {
+
+    /**
+     * The number of squares in the game board in the X direction.
+     */
+    private int numXSquares;
+    /**
+     * The number of squares in the game board in the Y direction.
+     */
+    private int numYSquares;
+    /**
+     * The total number of squares in the game board.
+     */
+    private int totalSquares;
+    /**
+     * A double array of CheckersSquares representing the entire game board.
+     */
+    private CheckersSquare[][] squaresList;
+    
+    /**
+     * Get total squares.
+     * @return totalSquares
+     */
+    public int getTotalSquares() {
+        return totalSquares;
+    }
+
+    /**
+     * Set total squares.
+     * @param totalSquares The total amount of squares.
+     */
+    public void setTotalSquares(final int totalSquares) {
+        this.totalSquares = totalSquares;
+    }
+
+    /**
+     * Get number of vertical squares.
+     * @return numYSquares
+     */
+    public int getNumYSquares() {
+        return numYSquares;
+    }
+
+    /**
+     * Set the number of vertical squares.
+     * @param numYSquares Total amount of vertical squares. 
+     */
+    public void setNumYSquares(final int numYSquares) {
+        this.numYSquares = numYSquares;
+    }
+
+    /**
+     * Get the number of horizontal squares.
+     * @return numXSquares
+     */
+    public int getNumXSquares() {
+        return numXSquares;
+    }
+
+    /**
+     * Sets the number of horizontal squares.
+     * @param numXSquares Number of horizontal squares.
+     */
+    public void setNumXSquares(final int numXSquares) {
+        this.numXSquares = numXSquares;
+    }
+
+    /**
+     * Returns the square list.
+     * @return squaresList
+     */
+    public CheckersSquare[][] getSquaresList() {
+        return squaresList;
+    }
+
+    /**
+     * Sets the square list.
+     * @param squaresList List of squares.
+     */
+    public void setSquaresList(final CheckersSquare[][] squaresList) {
+        this.squaresList = squaresList;
+    }
+
+    /**
+     * Abstract method to check if a given coordinate is within the
+     * bounds of the CheckersBoard.
+     * @param xLocation the x coordinate to check bounds with.
+     * @param yLocation the y coordinate to check bounds with.
+     * @return true if the coordinate is within the CheckersBoard,
+     * false if not.
+     */
+    abstract boolean inBoardBounds(int xLocation, int yLocation);
+
+    /**
+     * Represents Color values of black or white pieces and squares.
+     */
+    public enum Color {
+        /**
+         * The color white.
+         */
+        white,
+        /**
+         * The color black.
+         */
+        black;
+
+        /**
+         * Gives the opposite color of the calling Color enum.
+         * @return white if the color value is black; black if
+         * the color value is white.
+         */
+        public Color opposite() {
+            if (this == white) {
+                return black;
+            } else {
+                return white;
+            }
+        }
+    }
+}
---
+++ src/cis350/games/CheckersKing.java
@@ -0,0 +1,82 @@
+package cis350.games;
+
+import cis350.games.CheckersBoard.Color;
+
+/**
+ * Represents a King piece in Checkers.
+ */
+public class CheckersKing extends CheckersPiece {
+
+    /**
+     * Creates a new CheckersKing at the specified location.
+     * @param initX the x location of the king.
+     * @param initY the y location of the king.
+     * @param color the king's color.
+     * @param board reference to the game board the king is on.
+     */
+    public CheckersKing(
+            final int initX,
+            final int initY,
+            final Color color,
+            final CheckersStandardBoard board) {
+        super(initX, initY, color, board);
+        this.setNameOfPiece("king");
+    }
+
+    /**
+     * Detects if the move requested by the player is valid.
+     * @param newX the possible new x position of the king.
+     * @param newY the possible new y position of the king.
+     * @return true if it's a valid move, false if not.
+     */
+    @Override
+    boolean isValidSpecialMove(final int newX, final int newY) {
+        int xDisplacement = newX - getXLocation();
+        int yDisplacement = newY - getYLocation();
+        CheckersSquare squareToCheck =
+            getCurrentBoard()
+            .getSquaresList()[getXLocation() + xDisplacement]
+                    [getYLocation() + yDisplacement];
+        CheckersSquare squareToCheck2 =
+            getCurrentBoard()
+            .getSquaresList()[getXLocation() + xDisplacement]
+                    [getYLocation() + yDisplacement];
+        if (isValidKingMove(xDisplacement, yDisplacement)) {
+            if (Math.abs(yDisplacement) == 2) {
+                if (squareToCheck2.getIsOccupied()) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else if (Math.abs(yDisplacement) == 1) {
+                if (squareToCheck.getIsOccupied()) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Detects if the move requested by the player is valid for a king.
+     * @param xDisplacement the x location of the king.
+     * @param yDisplacement the y location of the king.
+     * @return true if the move is valid, false if not.
+     */
+    private boolean isValidKingMove(
+            final int xDisplacement,
+            final int yDisplacement) {
+        if (Math.abs(xDisplacement) == 2 && Math.abs(yDisplacement) == 2) {
+            return true;
+        } else if (Math.abs(xDisplacement) == 1
+                && Math.abs(yDisplacement) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
