ee54dd150cd1ba435f8c6be5dbfcbcfba699e9e2
ajmaley <maleya2mail.gvsu.edu>
2017-12-07 20:14:31 -0500

    Checkstyle changes

--- src/cis350/games/ChessGame.java
+++ src/cis350/games/ChessGame.java
@@ -102,6 +102,9 @@ public class ChessGame extends Observable {
     */
     private Stack<ChessMoveCommand> commandStack;
     
+    /**
+     * Holds current game type.
+     */
     private static int currGameType = -1;
 
 
@@ -156,8 +159,11 @@ public class ChessGame extends Observable {
         return response;
     }
 
-
-     private static void setGameType(int response) {
+    /**
+     * Sets the game type.
+     * @param response response
+     */
+     private static void setGameType(final int response) {
         currGameType = response;        
     }
 
@@ -380,34 +386,40 @@ public class ChessGame extends Observable {
         }
     }
     
+    /**
+     * Checks if achievement is earned for game type.
+     */
     private void checkForGameTypeAchievement() {
-        if(ChessGame.returnGameType() == 0) {
+        if (ChessGame.returnGameType() == 0) {
             this.setChanged();
             this.notifyObservers(Achievement.CHESS_COMPLETE_RANDOM_GAME);
             System.out.println("Completed Random Chess Game!");
-        }
-        else if(ChessGame.returnGameType() == 1) {
+        } else if (ChessGame.returnGameType() == 1) {
             this.setChanged();
             this.notifyObservers(Achievement.CHESS_COMPLETE_PEASANTS_GAME);
             System.out.println("Completed peasants chess game!");
-        }
-        else if(ChessGame.returnGameType() == 2) {
+        } else if (ChessGame.returnGameType() == 2) {
             this.setChanged();
             this.notifyObservers(Achievement.CHESS_COMPLETE_LEGALS_GAME);
             System.out.println("Completed legals chess game!");
-        }
-        else {
+        } else {
             return;
         }
         
     }
-
+    /**
+     * Returns game type.
+     * @return gameType
+     */
     private static int returnGameType() {
         return currGameType;
     }
-
-    private void checkForFirstWin(int score) {
-        if(score == 1) {
+    /**
+     * Checks for first win.
+     * @param score score of player
+     */
+    private void checkForFirstWin(final int score) {
+        if (score == 1) {
             this.setChanged();
             this.notifyObservers(Achievement.CHESS_FIRST_WIN);
         }
